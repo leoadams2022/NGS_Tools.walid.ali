75b4e9ae014e95f79372680ec04dea1ed2e9ca1f
walidWorkLap <sam0941535901@gmail.com>
2023-12-21 18:30:59 +0200

    21-12-2023

--- assets/Footer-n4sgYtXu.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-MK5WUaH3.js";function o(){return t.jsx("footer",{className:"hidden bg-background-100 text-text-100 border-t border-text-100 transition-colors duration-500 ease-in-out  shadow-md",children:t.jsx("h1",{className:"text-center text-2xl",children:"Footer"})})}export{o as default};
--- assets/Layout-UEKWlBqd.js
+++
@@ -1,7 +0,0 @@
-import{j as e,a as r,_ as o}from"./index-MK5WUaH3.js";import{A as t}from"./AuthContext-ztO8Oh6Q.js";import{O as a}from"./App-XAX-tNlb.js";const s=r.lazy(()=>o(()=>import("./Header-NBZV95Ms.js"),__vite__mapDeps([0,1,2,3]),import.meta.url)),i=r.lazy(()=>o(()=>import("./Footer-n4sgYtXu.js"),__vite__mapDeps([4,1,2]),import.meta.url));function n(){return e.jsx(e.Fragment,{children:e.jsxs("div",{className:"flex flex-col h-[100dvh] w-screen overflow-hidden",children:[e.jsx(s,{}),e.jsx("main",{className:"flex-1 overflow-auto",children:e.jsx(t,{children:e.jsx(a,{})})}),e.jsx(i,{})]})})}export{n as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./Header-NBZV95Ms.js","./index-MK5WUaH3.js","./index-oNAhUKS_.css","./App-XAX-tNlb.js","./Footer-n4sgYtXu.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
